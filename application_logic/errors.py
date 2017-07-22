2a94fe90f6373af13fd2c8ca0974aa26f78accd9
Valian <jakub.skalecki@gmail.com>
2017-07-22 21:47:36 +0200

    errors rewrite, removed not needed classes

--- application_logic/errors.py
+++ application_logic/errors.py
@@ -1,32 +1,22 @@
 from application_logic import exceptions
 from six import with_metaclass
 
-errors_registry = type('Errors', (), {})  # we setattr here all found errors
-
-
-class ErrorBase(object):
-    exception_class = None
-
-    def __init__(self, message):
-        self.message = message
-
-
-class NotPermitted(ErrorBase):
-    exception_class = exceptions.OperationNotPermittedException
-
-
-class InvalidOperation(ErrorBase):
-    exception_class = exceptions.InvalidOperationException
-
 
 class ServicesErrorsMetaclass(type):
+    """
+    Metaclass automatically creating errors registry and setting error code to attribute name.
+    You should subclass this and set all possible application logic exceptions.
+    """
     def __init__(cls, name, bases, dict):
         super(ServicesErrorsMetaclass, cls).__init__(name, bases, dict)
+        # dictionary containing all errors by error code
+        cls._errors = {}
         for attr_name, attr in dict.items():
-            if isinstance(attr, ErrorBase):
-                exception = attr.exception_class(attr.message, attr_name.lower(), cls)
-                setattr(errors_registry, attr_name, exception)
-                setattr(cls, attr_name, exception)
+            if isinstance(attr, exceptions.ServiceException):
+                # attribute name becomes exception `error_code`
+                attr.error_code = attr_name
+                attr.errors = cls._errors
+                cls._errors[attr_name] = attr
 
 
 class ServicesErrors(with_metaclass(ServicesErrorsMetaclass)):

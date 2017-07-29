59f1d1d9c58b0ab48edf14724d37fe3564634237
Valian <jakub.skalecki@gmail.com>
2017-07-29 17:29:17 +0200

    updated init and made metaclass private

--- business_logic/errors.py
+++ business_logic/errors.py
@@ -2,22 +2,22 @@ from business_logic import exceptions
 from six import with_metaclass
 
 
-class ServicesErrorsMetaclass(type):
+class _LogicErrorsMetaclass(type):
     """
     Metaclass automatically creating errors registry and setting error code to attribute name.
     You should subclass this and set all possible business logic exceptions.
     """
     def __init__(cls, name, bases, dict):
-        super(ServicesErrorsMetaclass, cls).__init__(name, bases, dict)
+        super(_LogicErrorsMetaclass, cls).__init__(name, bases, dict)
         # dictionary containing all errors by error code
         cls._errors = {}
         for attr_name, attr in dict.items():
-            if isinstance(attr, exceptions.ServiceException):
+            if isinstance(attr, exceptions.LogicException):
                 # attribute name becomes exception `error_code`
                 attr.error_code = attr_name
                 attr.errors = cls._errors
                 cls._errors[attr_name] = attr
 
 
-class ServicesErrors(with_metaclass(ServicesErrorsMetaclass)):
+class LogicErrors(with_metaclass(_LogicErrorsMetaclass)):
     pass

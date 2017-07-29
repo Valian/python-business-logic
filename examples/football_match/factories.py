6aeefb61108a98556b2da3c5b238a72665a45116
Valian <jakub.skalecki@gmail.com>
2017-07-29 18:05:24 +0200

    factories for example

---
+++ examples/football_match/factories.py
@@ -0,0 +1,46 @@
+import factory
+
+import models
+
+
+class PersonFactory(factory.Factory):
+
+    class Meta:
+        model = models.Person
+
+    name = factory.Faker('name')
+
+
+class RefereeFactory(PersonFactory):
+
+    class Meta:
+        model = models.Referee
+
+
+class PlayerFactory(PersonFactory):
+
+    class Meta:
+        model = models.Player
+
+    total_goals = 0
+
+
+class TeamFactory(factory.Factory):
+
+    class Meta:
+        model = models.Team
+
+    @factory.post_generation
+    def players(self, create, extracted, count=0, **kwargs):
+        if not extracted and count > 0:
+            self.players = [PlayerFactory(**kwargs) for _ in range(count)]
+
+
+class MatchFactory(factory.Factory):
+
+    class Meta:
+        model = models.Match
+
+    first_team = factory.SubFactory(TeamFactory, players__count=5)
+    second_team = factory.SubFactory(TeamFactory, players__count=5)
+    reward = 0

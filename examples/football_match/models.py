94f3383f7dc94995997eea86dd05d946e19afbb4
Valian <jakub.skalecki@gmail.com>
2017-07-30 13:52:07 +0200

    added string representation to models

--- examples/football_match/models.py
+++ examples/football_match/models.py
@@ -7,6 +7,9 @@ class Person(object):
     def is_referee(self):
         return False
 
+    def __str__(self):
+        return 'Person <name: "{}" cash: {} >'.format(self.name, self.cash)
+
 
 class Referee(Person):
 
@@ -14,11 +17,17 @@ class Referee(Person):
     def is_referee(self):
         return True
 
+    def __str__(self):
+        return 'Referee <name: "{}" cash: {} >'.format(self.name, self.cash)
+
 
 class Player(Person):
     def __init__(self, name, cash=0, total_goals=0):
         super(Player, self).__init__(name, cash)
         self.total_goals = total_goals
+
+    def __str__(self):
+        return 'Player <name: "{}" cash: {} goals: {}>'.format(self.name, self.cash, self.total_goals)
 
 
 class Team(object):
@@ -27,11 +36,18 @@ class Team(object):
         self.players = players or []
         self.goals = goals
 
+    def __str__(self):
+        return 'Team <name: "{}" goals: {} players_count: {} >'.format(self.name, self.goals, len(self.players))
+
 
 class Match(object):
 
     BEFORE_START, STARTED, FINISHED = range(3)
-    AVAILABLE_STATUSES = {BEFORE_START, STARTED, FINISHED}
+    AVAILABLE_STATUSES = {
+        BEFORE_START: 'Before start',
+        STARTED: 'Started',
+        FINISHED: 'Finished'
+    }
 
     def __init__(self, first_team, second_team, reward, status=BEFORE_START):
         self.first_team = first_team
@@ -39,3 +55,12 @@ class Match(object):
         self.reward = reward
         self.winner = None
         self.status = status
+
+    def get_status_display(self):
+        return self.AVAILABLE_STATUSES[self.status]
+
+    def __str__(self):
+        return 'Match "{} vs {}" <status: {}, reward: {}, winner: {}>'.format(
+            self.first_team.name, self.second_team.name, self.get_status_display(),
+            self.reward, self.winner and self.winner.name
+        )
